c87bb6353fa03596b6d8f0f783befb27f072828e
naima94 <naimatou.adambi@gmail.com>
2021-09-29 13:46:16 -0800

    Header 1

---
+++ components/Header.vue
@@ -0,0 +1,62 @@
+<template>
+  <div class="max-w-6xl mx-auto flex justify-between py-4 items-center">
+    <div class="flex space-x-3 items-center">
+      <span class="text-indigo-800 font-bold">OPENCLASSROOMS</span>
+      <div class="divide-x divide-gray-300 flex items-center">
+        <div class="px-3 flex items-center space-x-3 cursor-pointer">
+          <span>Training programms</span>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="12"
+            height="12"
+            viewBox="0 0 24 24"
+            style="fill: rgba(0, 0, 0, 1); transform: ; msfilter: "
+            class="hover:text-blue-800"
+          >
+            <path
+              d="M11.178 19.569a.998.998 0 0 0 1.644 0l9-13A.999.999 0 0 0 21 5H3a1.002 1.002 0 0 0-.822 1.569l9 13z"
+            ></path>
+          </svg>
+        </div>
+        <span class="px-3 cursor-pointer hover:text-blue-800"
+          >For business</span
+        >
+      </div>
+    </div>
+
+    <div class="flex space-x-3 items-center">
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="24"
+        height="24"
+        viewBox="0 0 24 24"
+        style="fill: rgba(0, 0, 0, 1); transform: ; msfilter: "
+      >
+        <path
+          d="M10 18a7.952 7.952 0 0 0 4.897-1.688l4.396 4.396 1.414-1.414-4.396-4.396A7.952 7.952 0 0 0 18 10c0-4.411-3.589-8-8-8s-8 3.589-8 8 3.589 8 8 8zm0-14c3.309 0 6 2.691 6 6s-2.691 6-6 6-6-2.691-6-6 2.691-6 6-6z"
+        ></path>
+      </svg>
+      <button
+        class="
+          py-2
+          px-4
+          rounded
+          border border-blue-600
+          uppercase
+          text-blue-800
+          font-bold
+          text-xs
+        "
+      >
+        sign in
+      </button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style>
+</style>
---
+++ components/Hero.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="h-96 bg-indigo-700"></div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style>
+</style>
